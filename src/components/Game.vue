d525ba4e777f714c31b53ad224902296bd6e0213
Carly Minjoy <cminjoy@myune.edu.au>
2019-08-12 19:09:51 +1000

    componetize

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -1,72 +1,106 @@
 <template>
   <div class="game">
-    <h1>Glock</h1>
+    <h1>Melody makers</h1>
 
     <svg width="800" height="350">
-      <rect
-        class="key c"
-        @click="addToMelody('c')"
-        x="0"
-        y="10"
-        height="350"
-        style="fill:#FF8484;"
+      <key
+        v-for="(settings, note) in notes"
+        v-on:play="userPlaysNote"
+        :id="note"
+        :key="note"
+        :note="settings"
       />
-      <rect class="key d" @click="addToMelody('d')" x="100" height="335" style="fill:#861388;" />
-      <rect class="key e" @click="addToMelody('e')" x="200" height="320" style="fill:#4DCCBD;" />
-      <rect class="key f" @click="addToMelody('f')" x="300" height="305" style="fill:#231651;" />
-      <rect class="key g" @click="addToMelody('g')" x="400" height="290" style="fill:#2374AB;" />
-      <rect class="key a" @click="addToMelody('a')" x="500" height="275" style="fill:#FFE74C;" />
-      <rect class="key b" @click="addToMelody('b')" x="600" height="260" style="fill:#FF5964;" />
-      <rect class="key c2" @click="addToMelody('c2')" x="700" height="245" style="fill:#A30B37;" />
     </svg>
 
     <div class="actions">
-      <button @click="playMelody(500)">Play</button>
-      <button @click="clearMelody()">Clear</button>
-      <button @click="createMelody(1000,8)">Random Melody</button>
+      <button v-if="!status.gameStarted" @click="startGame()">Go</button>
+      <button v-if="status.melodyReady && status.gameStarted" @click="listen()">Listen</button>
     </div>
 
-    <audio id="c" preload="auto">
-      <source src="c.wav" type="audio/wav" />
-    </audio>
-    <audio id="d" preload="auto">
-      <source src="d.wav" type="audio/wav" />
-    </audio>
-    <audio id="e" preload="auto">
-      <source src="e.wav" type="audio/wav" />
-    </audio>
-    <audio id="f" preload="auto">
-      <source src="f.wav" type="audio/wav" />
-    </audio>
-    <audio id="g" preload="auto">
-      <source src="g.wav" type="audio/wav" />
-    </audio>
-    <audio id="a" preload="auto">
-      <source src="a.wav" type="audio/wav" />
-    </audio>
-    <audio id="b" preload="auto">
-      <source src="b.wav" type="audio/wav" />
-    </audio>
-    <audio id="c2" preload="auto">
-      <source src="c2.wav" type="audio/wav" />
+    <h3>{{ status.message }}</h3>
+
+    <audio v-for="(settings, note) in notes" :id="note" preload="auto" :key="note">
+      <source :src="settings.sound" type="audio/wav" />
     </audio>
   </div>
 </template>
 
 <script>
+import { default as Key } from "./Key";
+import { default as notes } from "./../config/notes";
+
 export default {
   props: {
     msg: String
   },
+  components: {
+    Key
+  },
   data() {
     return {
       melody: [],
-      keys: ["c", "d", "e", "f", "g", "a", "b", "c2"]
+      guesses: [],
+      notes: notes,
+      status: {
+        message: "Ready?",
+        gameStarted: false,
+        melodyReady: false,
+        listening: false,
+        guessing: false,
+        adding: false,
+        gameOver: false
+      }
     };
   },
   methods: {
-    addToMelody(note) {
-      this.melody.push(note);
+    userPlaysNote(note) {
+      if (this.status.guessing) {
+        this.guesses.push(note);
+        this.play(note);
+
+        if (this.melody.length == this.guesses.length) {
+          this.status.guessing = false;
+          this.submitGuess();
+        }
+      } else if (this.status.adding) {
+        this.melody.push(note);
+        this.play(note);
+
+        this.status.message =
+          "Okay, here's the new melody! Waiting for Player 2 ...";
+        this.playMelody(1000);
+
+        // AI makes a move
+        setTimeout(() => {
+          this.aiMove(), 10000;
+        });
+      }
+    },
+    aiMove() {
+      this.melody.push(this.getRandomNote());
+      this.clearGuesses();
+      this.status.message = "Your turn!";
+      this.status.melodyReady = true;
+    },
+    submitGuess() {
+      let correct = true;
+
+      this.melody.forEach((note, i) => {
+        if (note != this.guesses[i]) {
+          correct = false;
+        }
+      });
+
+      if (correct) {
+        this.status.adding = true;
+        this.status.message = "Nice! Now add another note.";
+      } else {
+        this.status.gameOver = true;
+        this.status.message = "Too bad! Game over :(";
+      }
+    },
+    play(note) {
+      this.lightKey(note);
       this.playSound(note);
     },
     playSound(note) {
@@ -75,28 +109,55 @@ export default {
       audio.pause();
       audio.currentTime = 0;
       audio.play();
-
-      this.lightKey(note)
-
     },
     lightKey(note) {
-        let key = document.querySelector(`.key.${note}`);
-      let color = key.getAttribute("style");
-      key.setAttribute("style", "fill:#CED3DC");
-      setTimeout(() => key.setAttribute("style", color), 500);
+      this.notes[note].lit = true;
+      setTimeout(() => (this.notes[note].lit = false), 400);
+    },
+    reset() {
+      this.clearMelody();
+      this.clearGuesses();
+    },
+    clearStatus() {
+      this.status.melodyReady = false;
+      this.status.gameStarted = false;
+      this.status.listening = false;
+      this.status.guessing = false;
+      this.status.adding = false;
+      this.status.gameOver = false;
+      this.status.message = "Ready?";
+    },
+    startGame() {
+      this.status.gameStarted = true;
+      // Run AI until 2-player option available
+      this.createMelody(1000, 4);
     },
     createMelody(speed, count) {
-      this.melody = [];
+      this.reset();
+      this.status.message = "Listen and repeat.";
 
       for (let i = 0; i < count; i++) {
-        let randIndex = Math.floor(Math.random() * 8);
-        this.melody.push(this.keys[randIndex]);
+        this.melody.push(this.getRandomNote());
       }
 
-      this.playMelody(speed);
+      this.status.melodyReady = true;
+      console.log(this.melody);
+    },
+    listen() {
+      this.status.listening = true;
+      this.status.melodyReady = false;
+      this.playMelody(1000);
+    },
+    getRandomNote() {
+      let notes = ["c", "d", "e", "f", "g", "a", "b", "c2"];
+      let randIndex = Math.floor(Math.random() * 8);
+      return notes[randIndex];
     },
     clearMelody() {
       this.melody = [];
+    },
+    clearGuesses() {
+      this.guesses = [];
     },
     playMelody(speed) {
       let x = 0;
@@ -105,22 +166,20 @@ export default {
         if (!this.melody[x]) {
           clearInterval(intervalId);
         } else {
-          this.playSound(this.melody[x]);
+          this.play(this.melody[x]);
         }
 
         x++;
       }, speed);
-    }
-  },
-  mounted() {
-    let keys = document.querySelectorAll(".key");
 
-    keys.forEach(k => {
-      k.setAttribute("y", "0");
-      k.setAttribute("rx", "20");
-      k.setAttribute("ry", "20");
-      k.setAttribute("width", "80");
-    });
+      if (this.status.listening) {
+        setTimeout(() => {
+          this.status.message = "Now play it back!";
+          this.status.listening = false;
+          this.status.guessing = true;
+        }, speed * this.melody.length);
+      }
+    }
   }
 };
 </script>
@@ -128,14 +187,16 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
 div.game {
-
-    cursor: url("https://cdn.imgbin.com/13/17/18/imgbin-percussion-mallet-glockenspiel-drum-stick-xylophone-drum-stick-sVpUBXmd4ja6qqG77z1GyW1cx_t.jpg"), auto;
+  cursor: url("https://cdn.imgbin.com/13/17/18/imgbin-percussion-mallet-glockenspiel-drum-stick-xylophone-drum-stick-sVpUBXmd4ja6qqG77z1GyW1cx_t.jpg"),
+    auto;
 }
-svg {
-
-    .key {
-        width: 90px;
-        transition: 0.2s ease-in-out;
-    }
+button {
+  padding: 10px 15px;
+  margin: 10px;
+  font-size: 16px;
+  background: #eee;
+  text-transform: uppercase;
+  font-weight: bold;
+  border-radius: 4px;
 }
 </style>
---
+++ src/components/Key.vue
@@ -0,0 +1,58 @@
+<template>
+  <rect
+    class="key"
+    @click="play()"
+    :id="'note-' + id"
+    :x="x"
+    :y="y"
+    :rx="rx"
+    :ry="ry"
+    :width="width"
+    :height="height"
+    :style="'fill:' + color"
+  ></rect>
+</template>
+
+<script>
+export default {
+  props: {
+    id: String,
+    note: Object
+  },
+  computed: {
+    color() {
+      return this.note.lit ? "silver" : this.note.color;
+    }
+  },
+  data() {
+    return {
+      x: null,
+      y: 0,
+      rx: 20,
+      ry: 20,
+      height: null,
+      width: 80
+    };
+  },
+  methods: {
+    play() {
+      this.$emit("play", this.id);
+    }
+  },
+  mounted() {
+    this.x = this.note.order * 100;
+    this.height = 350 - this.note.order * 15;
+  }
+};
+</script>
+
+<style scoped lang='scss'>
+.key {
+  width: 90px;
+  transition: 0.2s ease-in-out;
+
+  &.lit {
+    fill: #555;
+  }
+}
+</style>
